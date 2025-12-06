3415c0860875e8bab16609c474ea4e7289b75650
Benjamin Brienen <Benjamin.Brienen.external@cpp.canon>
2025-12-06 15:09:39 +0100

    day 6 part 2

--- challenges/day_6/src/main.rs
+++ challenges/day_6/src/main.rs
@@ -30,35 +30,38 @@ fn solve(sequence: Sequence) -> u64 {
 fn parse_input(input_data: &str) -> Result<Sequence, String> {
     let mut lines = input_data.lines().rev();
     let final_line = lines.next().expect("the input data has a last line");
-    let operators: Vec<_> = final_line
-        .split_whitespace()
-        .map(|split| match split.trim() {
-            "+" => Operation::Addition,
-            "*" => Operation::Multiplication,
-            _ => unimplemented!(),
-        })
-        .collect();
-    let problems_count = operators.len();
-    let mut problems: Sequence = Vec::with_capacity(problems_count);
-    for (i, line) in lines
-        .inspect(|line| println!("line: {line}"))
+    let mut problems: Sequence = Vec::new();
+    let mut problem_index: usize = 0;
+    let operand_data: Vec<_> = lines
         .filter(|line| !line.is_empty())
-        .enumerate()
-    {
-        let splits: Vec<_> = line
-            .split_whitespace()
-            .inspect(|split| println!("split: {split}"))
-            .collect();
-        for j in 0..problems_count {
-            if i == 0 {
-                problems.push((Vec::new(), operators[j]));
+        .map(|line| line.chars().collect::<Vec<_>>())
+        .collect();
+    for (i, character) in final_line.chars().enumerate() {
+        if let Some(operation) = parse_operation(character) {
+            problems.push((Vec::new(), operation));
+        }
+        let mut operand: Operand = 0;
+        // luckily, cephalopod math doesn't have zeroes!
+        for j in 0..operand_data.len() {
+            if let Some(digit) = operand_data[operand_data.len() - j - 1][i].to_digit(10) {
+                operand = operand * 10 + digit as Operand;
             }
-            problems[j]
-                .0
-                .push(splits[j].parse().expect("input operands are numbers"));
+        }
+        if operand == 0 {
+            problem_index += 1;
+        } else {
+            problems[problem_index].0.push(operand);
         }
     }
     Ok(problems)
+}
+
+fn parse_operation(character: char) -> Option<Operation> {
+    match character {
+        '+' => Some(Operation::Addition),
+        '*' => Some(Operation::Multiplication),
+        _ => None,
+    }
 }
 
 type Sequence = Vec<(Vec<Operand>, Operation)>;
